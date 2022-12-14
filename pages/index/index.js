63239ebc973a45729b62a3592dff3eee26da1ece
drogunov <drogunov-s@yandex.ru>
2022-12-14 19:43:01 +0300

    refactor: Рефакторинг JS кода в index.js

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,41 +1,41 @@
-/* Открытие popup */
-let bottonEdit = document.querySelector('.profile__edit-botton');
-bottonEdit.addEventListener('click', openPopup);
+const popupOpened = 'popup_opened';
+
+let profileFullname = document.querySelector('.profile__full-name');
+let profilePosition = document.querySelector('.profile__position');
+let popup = document.querySelector('.popup');
+let inputName = document.querySelector('.popup__fullname');
+let inputPosition = document.querySelector('.popup__position');
+
+document
+  .querySelector('.popup__form')
+  .addEventListener('submit', handleFormSubmit);
+
+document
+  .querySelector('.profile__edit-botton')
+  .addEventListener('click', openPopup);
+
+document.querySelector('.popup__close')
+  .addEventListener('click', closePopup);
 
 function openPopup() {
-    let fullname = document.querySelector('.profile__full-name').textContent;
-    let position = document.querySelector('.profile__position').textContent;
-    let formFullname = document.querySelector('.popup__fullname');
-    let formPosition = document.querySelector('.popup__position');
-    formFullname.value = fullname;
-    formPosition.value = position;
-
-    let popupProfile = document.querySelector('.popup');
-    let classList = popupProfile.classList;
-    classList.add('popup_opened');
+  let fullname = profileFullname.textContent;
+  let position = profilePosition.textContent;
+  inputName.value = fullname;
+  inputPosition.value = position;
+  let classList = popup.classList;
+  classList.add(popupOpened);
 }
-
-
-document.querySelector('.popup__close').addEventListener('click', closePopup);
 
 function closePopup() {
-    let popupProfile = document.querySelector('.popup');
-    popupProfile.classList.remove('popup_opened');
+  popup.classList.remove(popupOpened);
+
 }
 
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__fullname');
-let jobInput = document.querySelector('.popup__position');
 function handleFormSubmit(evt) {
-    evt.preventDefault();
-    let updateFullname = nameInput.value;
-    let updatePosition = jobInput.value;
-    let profileName = document.querySelector('.profile__full-name');
-    profileName.textContent = updateFullname;
-    let profilePosition = document.querySelector('.profile__position');
-    profilePosition.textContent = updatePosition;
-    closePopup()
+  evt.preventDefault();
+  let updateFullname = inputName.value;
+  let updatePosition = inputPosition.value;
+  profileFullname.textContent = updateFullname;
+  profilePosition.textContent = updatePosition;
+  closePopup();
 }
-
-
-formElement.addEventListener('submit', handleFormSubmit);

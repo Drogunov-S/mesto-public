96cba45294dc9f464830b34756a2a979b23414bd
sergey <drogunov-s@yandex.ru>
2023-03-07 22:22:48 +0300

    hotfix: path in webpack config to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: '/src/index.html'
+      template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin()
